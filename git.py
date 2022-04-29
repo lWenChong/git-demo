50f5705ef69e6e98f0035c090e7049f05aaccd65
ytbryan <ytbryan@gmail.com>
2022-04-29 11:44:07 +0800

    a message

---
+++ git.py
@@ -0,0 +1,20 @@
+# Python program to explain os.system() method
+	
+# importing os module
+import os
+
+# Command to execute
+# Using Windows OS command
+# cmd = 'git'
+
+# # Using os.system() method
+# os.system(cmd)
+
+cmd1 = 'git add .'
+cmd2 = 'git commit -m "a message"'
+# cmd3 = 'git push'
+# Using os.system() method
+os.system(cmd1)
+os.system(cmd2)
+# os.system(cmd3)
+
